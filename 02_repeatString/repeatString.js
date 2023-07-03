c313d12b4f2e99d9b0c54bd0eddefeea77bbdc89
francisco <franjmarin1991@gmail.com>
2023-07-03 00:16:58 -0400

    complete exercise 02_repeatString
    passed all 7 test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    
+    if (number < 0) {
+        return "ERROR"
+    }
+     
+    let myString = "";
+    for (let i = 0; i <number; i++) {
+        myString += string;
+    }
+    return myString;
 };
 
 // Do not edit below this line
