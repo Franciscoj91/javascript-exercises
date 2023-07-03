9d663674fe746ececfdd2238d86d8840d020ddc0
francisco <franjmarin1991@gmail.com>
2023-07-03 17:00:25 -0400

    complete exercise 07_tempConversion
    passed all 6 tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemperature) {
+  let celsiusTemperature = (fahrenheitTemperature - 32) * 5/9;
+  return Math.round(celsiusTemperature *10) / 10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemperature) {
+  let fahrenheitTemperature = (celsiusTemperature * 9 / 5) + 32;
+  return Math.round(fahrenheitTemperature * 10) / 10; 
 };
 
 // Do not edit below this line
