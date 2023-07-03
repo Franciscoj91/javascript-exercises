4871466eb12377d7cf75e49d78b79a3249bfdd77
francisco <franjmarin1991@gmail.com>
2023-07-03 15:34:25 -0400

    complete exercise 04_removeFromArray
    passed all 7 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...args) {
+    for(let arg of args) {
+        let index = array.indexOf(arg);
+        if (index === -1) {
+            continue;
+        }
+        array.splice(index,1);
+    }
+    return array;
 };
 
 // Do not edit below this line
