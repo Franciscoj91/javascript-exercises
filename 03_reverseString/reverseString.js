25ed01e96718c8cad1b44f40989d45651b580f8b
francisco <franjmarin1991@gmail.com>
2023-07-03 14:41:08 -0400

    complete exercise 03_reverseString
    passed all 4 tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringAsArray = string.split('');
+    stringAsArray.reverse();
+    return stringAsArray.join('');
 };
 
 // Do not edit below this line
