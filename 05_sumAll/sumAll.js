15e514ce37049c8c8f2d63e9b7c90fe947f48be5
francisco <franjmarin1991@gmail.com>
2023-07-03 15:52:52 -0400

    complete exercise 05_sumAll
    passed all 6 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    if (firstNumber < 0 || secondNumber < 0 || typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
+        return "ERROR"
+    }
 
+    let sum = 0;
+
+    if ( firstNumber < secondNumber) {
+        for (let i = firstNumber; i <= secondNumber; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    
+    for (let i = secondNumber; i <= firstNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
